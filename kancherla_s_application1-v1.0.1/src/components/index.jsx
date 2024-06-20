e57a073776939a6b1b1367b7279b55cd8e61ce8a
KANCHERLA NAVEENKUMAR <136104163+naveenkumarkancherla@users.noreply.github.com>
2024-06-20 08:39:04 +0530

    Delete kancherla_s_application1-v1.0.1 directory

--- kancherla_s_application1-v1.0.1/src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "./Button";
-import { Img } from "./Img";
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Input } from "./Input";
-import { SelectBox } from "./SelectBox";
-export { Button, Img, Text, Heading, Input, SelectBox };
